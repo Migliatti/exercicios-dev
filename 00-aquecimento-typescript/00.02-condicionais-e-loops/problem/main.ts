26515f9ce19f650a487ff8bda4f3385970c696a7
Migliatti <gabimpariz@icloud.com>
2026-08-31 14:57:04 -0300

    refactor: clean up TODO comments in main.ts and add AGENTS.md file

--- 00-aquecimento-typescript/00.02-condicionais-e-loops/problem/main.ts
+++ 00-aquecimento-typescript/00.02-condicionais-e-loops/problem/main.ts
@@ -6,8 +6,7 @@ function classificarSeculo(ano: number) {
   // TODO: implemente aqui
 }
 
-// TODO 2: usando um for...of, percorra o array "anos" e, para cada ano,
-// imprima algo como "1899 -> século 19" (use classificarSeculo).
+// TODO 2: usando um for...of, percorra o array "anos" e, para cada ano, imprima algo como "1899 -> século 19" (use classificarSeculo).
 
 for (const ano of anos) {
   // TODO: implemente aqui
